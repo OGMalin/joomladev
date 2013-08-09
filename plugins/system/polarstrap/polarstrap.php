bc54c1c7fdf577963c79f77910fb0eefb5dbf34f
OGMalin <oddg@sjakk.biz>
2013-08-10 01:28:48 +0200

    Copy from 2.5 project.

---
+++ plugins/system/polarstrap/polarstrap.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @package     Joomla.Plugin
+ * @subpackage  System.Polarstrap
+ */
+
+defined('_JEXEC') or die;
+
+class plgSystemPolarstrap extends JPlugin
+{
+
+	public function onAfterDispatch()
+	{
+		$useInadmin = (int) $this->params->get('inadmin', false);
+		
+		if (!$useInadmin && JFactory::getApplication()->isAdmin())
+			return;
+		
+		$useCssfiles = (int) $this->params->get('cssfiles', false);
+		$useBootstrap = (int) $this->params->get('bootstrap', false);
+		$useJquery = (int) $this->params->get('jquery', false);
+		$useJquery_ui = (int) $this->params->get('jquery_ui', false);
+		$useNoconflict = (int) $this->params->get('noconflict', false);
+		
+		$doc = JFactory::getDocument();
+
+		if ($useCssfiles)
+			$doc->addStyleSheet('media/polarstrap/css/polarstrap.min.css');
+		if ($useJquery_ui)
+			$doc->addScript('media/polarstrap/js/jquery.min.js');
+		if ($useNoconflict)
+			$doc->addScript('media/polarstrap/js/jquery-noconflict.js');
+		if ($useJquery)
+			$doc->addScript('media/polarstrap/js/jquery-ui.min.js');
+		if ($useBootstrap)
+			$doc->addScript('media/polarstrap/js/bootstrap.min.js');
+	}
+}
