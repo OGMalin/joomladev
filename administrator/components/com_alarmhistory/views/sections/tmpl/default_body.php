22ad52fa9904149d804404c3b8c2c1ec61d23506
OGMalin <oddg@sjakk.biz>
2014-02-20 08:40:40 +0100

    *

---
+++ administrator/components/com_alarmhistory/views/sections/tmpl/default_body.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @package     Alarmhistory for Joomla 3.x
+ * @version     1.0.0
+ * @author      Odd Gunnar Malin
+ * @copyright   Copyright 2014. All rights reserved.
+ * @license     GNU General Public License version 2 or later.
+ */
+
+// No direct access
+defined('_JEXEC') or die;
+?>
+
+<?php foreach($this->items as $i => $item): ?>
+	<tr class="row<?php echo $i % 2; ?>">
+		<td>
+			<?php echo JHtml::_('grid.id', $i, $item->id); ?>
+		</td>
+		<td>
+		</td>
+		<td>
+			<?php echo $item->id; ?>
+		</td>
+	</tr>
+<?php endforeach; ?>
---
+++ administrator/components/com_alarmhistory/views/sections/tmpl/default_head.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @package     Alarmhistory for Joomla 3.x
+ * @version     1.0.0
+ * @author      Odd Gunnar Malin
+ * @copyright   Copyright 2014. All rights reserved.
+ * @license     GNU General Public License version 2 or later.
+ */
+
+// No direct access
+defined('_JEXEC') or die;
+?>
+<tr>
+	<th width="20">
+		<?php echo JHtml::_('grid.checkall');?>
+	</th>
+	<th>
+	</th>
+	<th>
+	</th>
+</tr>
