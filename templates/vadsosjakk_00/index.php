d92aad15e5060989dbabb98b5b30053118ce8e89
OGMalin <oddg@sjakk.biz>
2014-12-03 01:21:57 +0100

    Kopi av protostar

---
+++ templates/vadsosjakk_00/index.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * @version     $Id$
+ * @package     Joomla.Site
+ * @subpackage  templates.polartemplate
+ * @copyright   Copyright 2013. All rights reserved.
+ * @license     GNU General Public License version 2 or later.
+ */
+
+// No direct access
+defined('_JEXEC') or die;
+
+$app = JFactory::getApplication();
+$doc = JFactory::getDocument();
+$jversion = new JVersion();
+$version = $jversion->getShortVersion();
+
+$doc->addStyleSheet('templates/' . $this->template . '/css/template.css');
+if (JPath::find('templates/' . $this->template . '/css/', 'custom.css'))
+	$doc->addStyleSheet('templates/'.$this->template.'/css/custom.css');
+
+$useleft=($this->countModules('left')>0)?3:0;
+$useright=($this->countModules('right')>0)?3:0;
+$center='span'.(12-$useleft-$useright);
+
+// Earlier versions of Joomla don't have Bootstrap, use a plugin (Polarstrap) to enable bootstrap.
+if (version_compare($version, '3.0', 'ge'))
+	JHtml::_('bootstrap.framework');
+
+//JHtml::_('behavior.modal');
+// $doc->addScript( $this->baseurl . '/templates/' . $this->template . '/js/jquery.min.js');
+// $doc->addScript( $this->baseurl . '/templates/' . $this->template . '/js/bootstrap.min.js');
+// $doc->addScript( $this->baseurl . '/templates/' . $this->template . '/js/jquery-noconflict.js');
+// $doc->addScript( $this->baseurl . '/templates/' . $this->template . '/js/jquery-ui.min.js');
+
+?>
+<!DOCTYPE html>
+<html lang="<?php echo $this->language; ?>" >
+<head>
+	<meta charset="utf-8">
+	<jdoc:include type="head" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+ 	<!--[if lt IE 9]>
+		<script src="media/jui/js/html5.js"></script>
+	<![endif]-->
+</head>
+<body>
+	<div class="container">
+		<?php if ($this->countModules('top')) : ?>
+			<div class="row">
+				<jdoc:include type="modules" name="top" style="xhtml" />
+			</div>
+		<?php endif ?>
+		<div class="row">
+			<?php if ($useleft) : ?>
+		  	<div class="span3">
+			 		<jdoc:include type="modules" name="left" style="xhtml" />
+		 		</div>
+		  <?php endif ?>
+		  <div class="<?php echo $center ?>">
+		  	<?php if ($this->countModules('above')) : ?>
+			 		<jdoc:include type="modules" name="above" style="xhtml" />
+				<?php endif ?>
+				<jdoc:include type="message" />
+				<jdoc:include type="component" />
+			 	<?php if ($this->countModules('below')) : ?>
+			 		<jdoc:include type="modules" name="below" style="xhtml" />
+				<?php endif ?>
+			</div>
+			<?php if ($useright) : ?>
+			  <div class="span3">
+					<jdoc:include type="modules" name="right" style="xhtml" />
+				</div>
+			<?php endif ?>
+		</div>
+	 	<?php if ($this->countModules('bottom')) : ?>
+		 	<div class="row">
+				<jdoc:include type="modules" name="bottom" style="xhtml" />
+			</div>
+	 	<?php endif ?>
+	 	<?php if ($this->countModules('footer')) : ?>
+			<div class="row">
+				<jdoc:include type="modules" name="footer" style="xhtml" />
+			</div>
+	 	<?php endif ?>
+	</div>
+	<jdoc:include type="modules" name="debug" style="xhtml" />
+</body>
+</html>
