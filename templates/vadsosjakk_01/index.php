9f4423f8a1edb335b9f737e9f6ede515868f92df
OGMalin <oddg@sjakk.biz>
2014-12-03 15:03:38 +0100

    Grunnarbeid

--- templates/vadsosjakk_01/index.php
+++ templates/vadsosjakk_01/index.php
@@ -5,14 +5,16 @@
 *
 * @copyright Copyright (C) 2014.
 */
+
+// Ingen direkte aksess
 defined('_JEXEC') or die;
+
 $app = JFactory::getApplication();
 $doc = JFactory::getDocument();
 $user = JFactory::getUser();
 $this->language = $doc->language;
-$this->direction = $doc->direction;
 
-// Getting params from template
+// Hen parametrer som er satt opp
 $params = $app->getTemplate(true)->params;
 
 // Detecting Active Variables
@@ -30,82 +32,93 @@ if($task == "edit" || $layout == "form" )
 	$fullWidth = 0;
 }
 
-// Add JavaScript Frameworks
+// Legg til JavaScript Frameworks
 JHtml::_('bootstrap.framework');
-//$doc->addScript($this->baseurl . '/templates/' . $this->template . '/js/template.js');
 
-// Add Stylesheets
+// Egne script
+$doc->addScript($this->baseurl . '/templates/' . $this->template . '/js/template.js');
+
+// Legg til Stylesheets
 $doc->addStyleSheet($this->baseurl . '/templates/' . $this->template . '/css/template.css');
+if (JPath::find($this->baseurl . 'templates/' . $this->template . '/css/', 'custom.css'))
+	$doc->addStyleSheet($this->baseurl . 'templates/'.$this->template.'/css/custom.css');
 
-// Adjusting content width
-if ($this->countModules('right') && $this->countModules('left'))
-{
-	$span = "span6";
-}elseif ($this->countModules('right') && !$this->countModules('left'))
-{
-	$span = "span9";
-}elseif (!$this->countModules('right') && $this->countModules('left'))
-{
-	$span = "span9";
-}else
-{
-	$span = "span12";
-}
+// Finn bredden på Innholdsboksen
+$useleft=($this->countModules('left')>0)?3:0;
+$useright=($this->countModules('right')>0)?3:0;
+$center='span'.(12-$useleft-$useright);
+
 ?>
 <!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>">
+<html lang="<?php echo $this->language; ?>" >
 	<head>
-		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<meta charset="utf-8">
 		<jdoc:include type="head" />
-		<!--[if lt IE 9]>
-			<script src="<?php echo $this->baseurl; ?>/media/jui/js/html5.js"></script>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+ 		<!--[if lt IE 9]>
+			<script src="media/jui/js/html5.js"></script>
 		<![endif]-->
 	</head>
+
 	<body>
 		<div class="body">
-			<div class="container>
-			<header class="header">
-				<div class="header-inner clearfix">
-					<div class="header-search pull-right">
-						<jdoc:include type="modules" name="search" style="none" />
-					</div>
-				</div>
-			</header>
-				<?php if ($this->countModules('navigation')) : ?>
-				<nav class="navigation" role="navigation">
-					<jdoc:include type="modules" name="navigation" style="none" />
-				</nav>
-				<?php endif; ?>
-				<jdoc:include type="modules" name="banner" style="xhtml" />
+			<div class="container">
+				<?php if ($this->countModules('banner')) : ?>
 				<div class="row">
-					<?php if ($this->countModules('left')) : ?>
-					<div id="sidebar" class="span3">
-						<div class="sidebar-nav">
-							<jdoc:include type="modules" name="left" style="xhtml" />
-						</div>
-					</div>
-					<?php endif; ?>
-					<main id="content" class="<?php echo $span; ?>">
-						<jdoc:include type="modules" name="top" style="xhtml" />
+					<jdoc:include type="modules" name="banner" style="none" />
+				</div>
+				<?php endif ?>
+				<?php if ($this->countModules('navigation')) : ?>
+				<div class="row">
+					<jdoc:include type="modules" name="navigation" style="none" />
+				</div>
+				<?php endif ?>
+				<?php if ($this->countModules('top')) : ?>
+				<div class="row">
+					<jdoc:include type="modules" name="top" style="none" />
+				</div>
+				<?php endif ?>
+				<?php if ($this->countModules('breadcrumb')) : ?>
+				<div class="row">
+					<jdoc:include type="modules" name="breadcrumb" style="none" />
+				</div>
+				<?php endif ?>
+				
+				<div class="row">
+					<?php if ($useleft) : ?>
+		  		<div class="span3">
+			 			<jdoc:include type="modules" name="left" style="none" />
+		 			</div>
+		  		<?php endif ?>
+		  		<div class="<?php echo $center ?>">
+		  			<?php if ($this->countModules('above')) : ?>
+			 			<jdoc:include type="modules" name="above" style="none" />
+						<?php endif ?>
 						<jdoc:include type="message" />
 						<jdoc:include type="component" />
-						<jdoc:include type="modules" name="breadcrumb" style="none" />
-					</main>
-					<?php if ($this->countModules('right')) : ?>
-					<div id="aside" class="span3">
-						<jdoc:include type="modules" name="right" style="well" />
+			 			<?php if ($this->countModules('below')) : ?>
+			 			<jdoc:include type="modules" name="below" style="none" />
+						<?php endif ?>
 					</div>
-					<?php endif; ?>
+					<?php if ($useright) : ?>
+			  	<div class="span3">
+						<jdoc:include type="modules" name="right" style="none" />
+					</div>
+					<?php endif ?>
+				</div>
+				<?php if ($this->countModules('bottom')) : ?>
+				<div class="row">
+					<jdoc:include type="modules" name="bottom" style="none" />
+				</div>
+				<?php endif ?>
+	 			<?php if ($this->countModules('footer')) : ?>
+				<div class="row">
+					<jdoc:include type="modules" name="footer" style="none" />
+				</div>
+	 			<?php endif ?>
 				</div>
 			</div>
+			<jdoc:include type="modules" name="debug" style="none" />
 		</div>
-		<footer class="footer">
-			<div class="container">
-				<hr />
-				<jdoc:include type="modules" name="footer" style="none" />
-				<p>&copy; <?php echo date('Y'); ?> <?php echo $sitename; ?></p>
-			</div>
-		</footer>
-		<jdoc:include type="modules" name="debug" style="none" />
 	</body>
 </html>
