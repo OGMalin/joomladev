79d8a71bba231d658ae169a65edbe2e89b6f56a4
OGMalin <oddg@sjakk.biz>
2015-02-18 13:26:23 +0100

    Overide of frontend editing

--- templates/vadsosjakk/html/com_content/form/edit.php
+++ templates/vadsosjakk/html/com_content/form/edit.php
@@ -3,7 +3,7 @@
  * @package     Joomla.Site
  * @subpackage  com_content
  *
- * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
+ * @copyright   Copyright (C) 2005 - 2014 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
@@ -12,7 +12,7 @@ defined('_JEXEC') or die;
 JHtml::_('behavior.tabstate');
 JHtml::_('behavior.keepalive');
 JHtml::_('behavior.calendar');
-JHtml::_('behavior.formvalidator');
+JHtml::_('behavior.formvalidation');
 JHtml::_('formbehavior.chosen', 'select');
 JHtml::_('behavior.modal', 'a.modal_jform_contenthistory');
 
@@ -27,18 +27,18 @@ if (!$editoroptions)
 {
 	$params->show_urls_images_frontend = '0';
 }
+?>
 
-JFactory::getDocument()->addScriptDeclaration("
+<script type="text/javascript">
 	Joomla.submitbutton = function(task)
 	{
 		if (task == 'article.cancel' || document.formvalidator.isValid(document.getElementById('adminForm')))
 		{
-			" . $this->form->getField('articletext')->save() . "
+			<?php echo $this->form->getField('articletext')->save(); ?>
 			Joomla.submitform(task);
 		}
 	}
-");
-?>
+</script>
 <div class="edit item-page<?php echo $this->pageclass_sfx; ?>">
 	<?php if ($params->get('show_page_heading', 1)) : ?>
 	<div class="page-header">
@@ -48,16 +48,16 @@ JFactory::getDocument()->addScriptDeclaration("
 	</div>
 	<?php endif; ?>
 
-	<form action="<?php echo JRoute::_('index.php?option=com_content&a_id=' . (int) $this->item->id); ?>" method="post" name="adminForm" id="adminForm" class="form-validate form-vertical">
+	<form action="<?php echo JRoute::_('index.php?option=com_content&a_id='.(int) $this->item->id); ?>" method="post" name="adminForm" id="adminForm" class="form-validate form-vertical">
 		<div class="btn-toolbar">
 			<div class="btn-group">
 				<button type="button" class="btn btn-primary" onclick="Joomla.submitbutton('article.save')">
-					<span class="icon-ok"></span><?php echo JText::_('JSAVE') ?>
+					<span class="icon-ok"></span>&#160;<?php echo JText::_('JSAVE') ?>
 				</button>
 			</div>
 			<div class="btn-group">
 				<button type="button" class="btn" onclick="Joomla.submitbutton('article.cancel')">
-					<span class="icon-cancel"></span><?php echo JText::_('JCANCEL') ?>
+					<span class="icon-cancel"></span>&#160;<?php echo JText::_('JCANCEL') ?>
 				</button>
 			</div>
 			<?php if ($params->get('save_history', 0)) : ?>
@@ -69,9 +69,6 @@ JFactory::getDocument()->addScriptDeclaration("
 		<fieldset>
 			<ul class="nav nav-tabs">
 				<li class="active"><a href="#editor" data-toggle="tab"><?php echo JText::_('COM_CONTENT_ARTICLE_CONTENT') ?></a></li>
-				<?php if ($params->get('show_urls_images_frontend') ) : ?>
-				<li><a href="#images" data-toggle="tab"><?php echo JText::_('COM_CONTENT_IMAGES_AND_URLS') ?></a></li>
-				<?php endif; ?>
 				<li><a href="#publishing" data-toggle="tab"><?php echo JText::_('COM_CONTENT_PUBLISHING') ?></a></li>
 			</ul>
 
@@ -82,8 +79,47 @@ JFactory::getDocument()->addScriptDeclaration("
 					<?php if (is_null($this->item->id)) : ?>
 						<?php echo $this->form->renderField('alias'); ?>
 					<?php endif; ?>
+
 					<?php echo $this->form->getInput('articletext'); ?>
 				</div>
+				<?php if ($params->get('show_urls_images_frontend')): ?>
+				<div class="tab-pane" id="images">
+					<?php echo $this->form->renderField('image_intro', 'images'); ?>
+					<?php echo $this->form->renderField('image_intro_alt', 'images'); ?>
+					<?php echo $this->form->renderField('image_intro_caption', 'images'); ?>
+					<?php echo $this->form->renderField('float_intro', 'images'); ?>
+					<?php echo $this->form->renderField('image_fulltext', 'images'); ?>
+					<?php echo $this->form->renderField('image_fulltext_alt', 'images'); ?>
+					<?php echo $this->form->renderField('image_fulltext_caption', 'images'); ?>
+					<?php echo $this->form->renderField('float_fulltext', 'images'); ?>
+					<?php echo $this->form->renderField('urla', 'urls'); ?>
+					<?php echo $this->form->renderField('urlatext', 'urls'); ?>
+					<div class="control-group">
+						<div class="controls">
+							<?php echo $this->form->getInput('targeta', 'urls'); ?>
+						</div>
+					</div>
+					<?php echo $this->form->renderField('urlb', 'urls'); ?>
+					<?php echo $this->form->renderField('urlbtext', 'urls'); ?>
+					<div class="control-group">
+						<div class="controls">
+							<?php echo $this->form->getInput('targetb', 'urls'); ?>
+						</div>
+					</div>
+					<?php echo $this->form->renderField('urlc', 'urls'); ?>
+					<?php echo $this->form->renderField('urlctext', 'urls'); ?>
+					<div class="control-group">
+						<div class="controls">
+							<?php echo $this->form->getInput('targetc', 'urls'); ?>
+						</div>
+					</div>
+					<?php echo $this->form->renderField('tags'); ?>
+					<?php if ($params->get('save_history', 0)) : ?>
+						<?php echo $this->form->renderField('version_note'); ?>
+					<?php endif; ?>
+					<?php echo $this->form->renderField('created_by_alias'); ?>
+				</div>
+				<?php endif; ?>
 				<div class="tab-pane" id="publishing">
 					<?php echo $this->form->renderField('catid'); ?>
 					<?php if ($this->item->params->get('access-change')) : ?>
@@ -101,6 +137,19 @@ JFactory::getDocument()->addScriptDeclaration("
 								<?php echo JText::_('COM_CONTENT_ORDERING'); ?>
 							</div>
 						</div>
+					<?php endif; ?>
+				</div>
+				<div class="tab-pane" id="language">
+					<?php echo $this->form->renderField('language'); ?>
+				</div>
+				<div class="tab-pane" id="metadata">
+					<?php echo $this->form->renderField('metadesc'); ?>
+					<?php echo $this->form->renderField('metakey'); ?>
+
+					<input type="hidden" name="task" value="" />
+					<input type="hidden" name="return" value="<?php echo $this->return_page; ?>" />
+					<?php if ($this->params->get('enable_category', 0) == 1) :?>
+					<input type="hidden" name="jform[catid]" value="<?php echo $this->params->get('catid', 1); ?>" />
 					<?php endif; ?>
 				</div>
 			</div>
